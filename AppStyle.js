83e5d2c8ba6bebfe897599e1afa65e4b1eb3e579
partpia <partanenppia@gmail.com>
2022-04-21 15:51:54 +0300

    File for styling

---
+++ AppStyle.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#F3F3E7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputContainer: {
+        marginTop: 120,
+        width: '80%',
+    },
+    input: {
+        backgroundColor: 'white',
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        width: '100%',
+        marginBottom: 20,
+        paddingVertical: 5,
+        alignItems: 'center',
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    logo: {
+        textAlign: 'center',
+        fontWeight: '700',
+    },
+
+})
