aace135bc07d4e8ac2ffb67180f291a9b8eb4877
partpia <partanenppia@gmail.com>
2022-04-27 17:48:52 +0300

    Some styling

--- AppStyle.js
+++ AppStyle.js
@@ -38,5 +38,83 @@ export default StyleSheet.create({
         textAlign: 'center',
         fontWeight: '700',
     },
-
+    searchView: {
+        flex: 1,
+        backgroundColor: '#3AA3A0',
+        width: '100%'
+    },
+    categoryCardGroup: {
+        flex: 2,
+    },
+    categoryCard: {
+        width: 250,
+    },
+    categoryCardText: {
+        textTransform: 'capitalize',
+    },
+    searchResultText: {
+        flex: 1,
+        backgroundColor: '#F8EA8C',
+        width: '100%',
+    },
+    keywordStyle: {
+        textTransform: 'lowercase',
+        color: '#3AA3A0',
+        fontWeight: '700',
+        fontSize: 26,
+    },
+    searchResultList: {
+        flex: 2,
+        padding: 10,
+    },
+    searcedRecipesListItems: {
+        alignItems: 'center',
+    },
+    searcedRecipesListImg: {
+        width: 140,
+        height: 140,
+        borderRadius: 10,
+        margin: 5,
+    },
+    searcedRecipesListText: {
+        fontSize: 20,
+    },
+    modalCentered: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    ingredientsChips: {
+        justifyContent: 'center',
+        backgroundColor: '#F6C324',
+        margin: 5,
+    },
+    test: {
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    dialog: {
+        flex: 1,
+        backgroundColor: 'green',
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    recipeDetails: {
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    recipeImg: {
+        height: 300,
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    divider: {
+        borderWidth: 1,
+        borderColor: '#3AA3A0',
+        marginBottom: 20,
+        marginTop: 20,
+        width: '100%'
+    },
 })
