5219e88b97a7ebc22e5aaa6e25cb858d5ebb20d2
partpia <partanenppia@gmail.com>
2022-05-16 22:42:27 +0300

    Changed how sections are displayed in view

--- screens/RecipeSearchScreen.js
+++ screens/RecipeSearchScreen.js
@@ -12,57 +12,24 @@ import styles from '../AppStyle';
 const RecipeSearchScreen = ({ route, navigation }) => {
     const { word } = route.params;
     const [searchedRecipes, setSearchedRecipes] = useState([]);
-    const [nextResultsUrl, setNextResultsUrl] = useState('');
     const [keyword, setKeyword] = useState('');
 
     useEffect(() => {
         setKeyword(word);
-        searchRecipesByKeyword(word)
+        //searchRecipesByKeyword(word)
     }, [word]);
 
+    // fetches recipes by keyword from Edamam recipe API (https://developer.edamam.com/edamam-recipe-api)
     const searchRecipesByKeyword = (word) => {
         try {
             fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${word}&app_id=${appid}&app_key=${apikey}&mealType=Dinner&field=label&field=image&field=url&field=ingredients&field=mealType`)
                 .then(response => response.json())
                 .then(data => {
                     setSearchedRecipes(data.hits);
-                    setNextResultsUrl(data._links.next.href);
                     console.log('nyt');
                 })
         } catch (error) {
             console.log(error);
-        }
-    }
-
-    const listSeparator = () => {
-        return (
-            <View
-                style={{
-                    height: 2,
-                    width: "100%",
-                    backgroundColor: "#3AA3A0",
-                }}
-            />
-        );
-    };
-    // onEndReached={loadMore} TODO: not working yet
-    const loadMore = () => {
-        if (nextResultsUrl != undefined) {
-            const tempList = [];
-            try {
-                fetch(nextResultsUrl)
-                    .then(response => response.json())
-                    .then(data => {
-                        setNextResultsUrl(data._links.next.href);
-                        data.hits.forEach(element => {
-                            tempList.push(element);
-                            console.log(element.recipe.label);
-                        });
-                    })
-                //setSearchedRecipes([...searchedRecipes, tempList]);
-            } catch (error) {
-                console.log(error);
-            }
         }
     }
 
@@ -92,7 +59,6 @@ const RecipeSearchScreen = ({ route, navigation }) => {
                 <FlatList
                     data={searchedRecipes}
                     keyExtractor={(item, index) => index}
-                    ItemSeparatorComponent={listSeparator}
                     renderItem={({ item, index }) =>
                             <FlatList
                                 data={searchedRecipes}
--- screens/ShoppingItemsScreen.js
+++ screens/ShoppingItemsScreen.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { ScrollView, View } from 'react-native';
-import { Button, Divider, List } from 'react-native-paper';
+import { Button, Divider, Headline, List, Subheading } from 'react-native-paper';
 import { auth, db } from "../firebase";
-import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query } from "firebase/firestore";
 import styles from '../AppStyle';
 import AddItemToList from "../components/AddItemToList";
+import { showToast } from "../components/Toast";
 
 const ShoppingItemsScreen = ({ route }) => {
     const { listId } = route.params;
     const [items, setItems] = useState([]);
     const [itemsPickedUp, setItemsPickedUp] = useState([]);
     const [sections, setSections] = useState([]);
+    const [noItems, setNoItems] = useState(false);
 
     useEffect(() => {
         getSections();
         getShoppingListItems();
     }, [])
 
+    // gets sections that user has saved
     const getSections = async () => {
         try {
             const tempSections = [];
@@ -30,69 +33,92 @@ const ShoppingItemsScreen = ({ route }) => {
             });
             setSections(tempSections);
         } catch (error) {
-            console.log(error);
+            showToast('Failed to get sections')
         }
     }
-
+    // gets selected shopping list's items
     const getShoppingListItems = async () => {
         try {
             const docRef = doc(db, `items/${listId}`);
             const docSnap = await getDoc(docRef);
+            const active = docSnap.data().active;
+            const inactive = docSnap.data().inactive;
 
             if (docSnap.exists()) {
-                setItems(docSnap.data().active);
-                setItemsPickedUp(docSnap.data().inactive)
-            } else {
-                console.log("No such document!");
+                if (active.length === 0 && inactive.length === 0) {
+                    setNoItems(true);
+                }
+                else {
+                    setItems(active);
+                    setItemsPickedUp(inactive)
+                }
             }
         } catch (error) {
-            console.log(error)
+            showToast('Failed to get items')
         }
     }
-
+    // moves item from list to 'picked up'-list
     const removeFromList = (item) => {
         setItemsPickedUp([...itemsPickedUp, item])
         const tempList = items.filter(obj => obj !== item);
         setItems(tempList);
     }
-
-    const addBackToList = (item) => {
+    // moves item from 'picked up'-list to list
+    const addToList = (item) => {
         setItems([...items, item])
         const tempList = itemsPickedUp.filter(obj => obj !== item);
         setItemsPickedUp(tempList);
     }
 
     return (
-        <View style={styles.shoppingItemsContainer}>
-            {sections.map((section) => (
-                <List.Section title={section.name} titleStyle={styles.sectionTitle} key={section.id}>
-                    {items &&
-                        items.filter(active => active.section === section.id).map((item, index) => (
+        <ScrollView style={styles.shoppingItemsContainer}>
+            {noItems ? (
+                <View style={styles.noShoppingListView}>
+                    <Headline>Nothing in the list yet!</Headline>
+                    <Subheading>Add first one</Subheading>
+                </View>
+            ) : (
+                <View>
+                    {sections.map((section) => (
+                        <List.Section title={section.name} titleStyle={styles.sectionTitle} key={section.id}>
+                            {items &&
+                                items.filter(active => active.section === section.id).map((item, index) => (
+                                    <List.Item
+                                        title={item.food + '  ' + item.quantity + ' ' + item.measure}
+                                        titleStyle={styles.itemTitle}
+                                        left={props => <List.Icon {...props} icon='checkbox-multiple-blank-circle-outline' key={index} />}
+                                        onPress={() => removeFromList(item)}
+                                        key={index} />
+                                ))
+                            }
+                            <Divider />
+                        </List.Section>
+                    ))
+                    }
+                    <List.Section title="Recipes" titleStyle={styles.sectionTitle}>
+                        {items.filter(active => active.section === 'recipes').map((item, index) => (
                             <List.Item
                                 title={item.food + '  ' + item.quantity + ' ' + item.measure}
                                 titleStyle={styles.itemTitle}
                                 left={props => <List.Icon {...props} icon='checkbox-multiple-blank-circle-outline' key={index} />}
                                 onPress={() => removeFromList(item)}
                                 key={index} />
-                        ))
-                    }
-                    <Divider />
-                </List.Section>
-            ))}
-            <List.Section title="Picked up" titleStyle={styles.sectionTitle}>
-                {itemsPickedUp &&
-                    itemsPickedUp.map((item, index) => (
-                        <List.Item
-                            title={item.food + '  ' + item.quantity + ' ' + item.measure}
-                            titleStyle={styles.itemsPickedUp}
-                            left={props => <List.Icon {...props} icon='checkbox-multiple-marked-circle-outline' key={index} />}
-                            onPress={() => addBackToList(item)}
-                            key={index} />
-                    ))
-                }
-            </List.Section>
-            <AddItemToList listId={listId} sections={sections} />
-        </View>
+                        ))}
+                    </List.Section>
+                    <List.Section title="Picked up" titleStyle={styles.sectionTitle}>
+                        {itemsPickedUp.map((item, index) => (
+                            <List.Item
+                                title={item.food + '  ' + item.quantity + ' ' + item.measure}
+                                titleStyle={styles.itemsPickedUp}
+                                left={props => <List.Icon {...props} icon='checkbox-multiple-marked-circle-outline' key={index} />}
+                                onPress={() => addToList(item)}
+                                key={index} />
+                        ))}
+                    </List.Section>
+                </View>)}
+                <Button>Delete picked ups</Button>
+            <AddItemToList listId={listId} sections={sections} getShoppingListItems={getShoppingListItems} />
+        </ScrollView>
     );
 }
 export default ShoppingItemsScreen;
